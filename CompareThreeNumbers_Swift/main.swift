22b2dbaca552483179103caa5cb6e7a7896f86b4
leonguevara <leon.guevara@outlook.com>
2017-05-30 11:51:08 -0500

    Initial Commit

---
+++ CompareThreeNumbers_Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CompareThreeNumbers_Swift
+//
+//  Created by León Felipe Guevara Chávez on 5/30/17.
+//  Copyright © 2017 León Felipe Guevara Chávez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
